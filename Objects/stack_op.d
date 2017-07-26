72be36b3cb0243e22d2f17748c91902d8831dc57
Joshua Shireman <kealist@gmail.com>
2017-07-26 11:44:35 -0400

    Remove old object files

--- Objects/stack_op.d
+++
@@ -1,1 +0,0 @@
-.\objects\stack_op.o: ..\..\..\Downloads\Lab-src-code-v2\Lab_src_code\lecture_29\src\stack_op.s
--- Objects/startup_stm32f723xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f723xx.o: RTE\Device\STM32F723ICKx\startup_stm32f723xx.s
